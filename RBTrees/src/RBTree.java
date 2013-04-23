251e21023f34a384dae29c9788c691f5e5d63ffd
slallum <horowitz.ofer@gmail.com>
2013-04-23 17:22:30 +0300

    added min/max implementation

--- RBTrees/src/RBTree.java
+++ RBTrees/src/RBTree.java
@@ -2,139 +2,144 @@
  * 
  * RBTree
  * 
- * An implementation of a Red Black Tree with
- * non-negative, distinct integer values
+ * An implementation of a Red Black Tree with non-negative, distinct integer
+ * values
  * 
  */
 
 public class RBTree {
 
-  /**
-   * public boolean empty()
-   * 
-   * returns true if and only if the tree is empty
-   *  
-   */
-  public boolean empty() {
-    return false; // to be replaced by student code
-  }
+	/**
+	 * public boolean empty()
+	 * 
+	 * returns true if and only if the tree is empty
+	 * 
+	 */
+	public boolean empty() {
+		return false; // to be replaced by student code
+	}
 
- /**
-   * public boolean search(int i)
-   * 
-   * returns i if the tree contains i
-   *  otherwise, the closest value to i (in terms of absolute difference)
-   */
-  public int search(int i)
-  {
-	return 0;  // to be replaced by student code
-  }
+	/**
+	 * public boolean search(int i)
+	 * 
+	 * returns i if the tree contains i otherwise, the closest value to i (in
+	 * terms of absolute difference)
+	 */
+	public int search(int i) {
+		return 0; // to be replaced by student code
+	}
 
-  /**
-   * public void insert(int i)
-   * 
-   * inserts the integer i into the binary tree; the tree
-   * must remain valid (keep its invariants).
-   * the function returns the number of rotations, or 0 if i was already in the tree
-   * or no reotations were needed
-   */
-   public int insert(int i) {
-	  return 0;	// to be replaced by student code
-   }
-  
-  /**
-   * public void delete(int i)
-   * 
-   * deletes the integer i from the binary tree, if it is there;
-   * the tree must remain valid (keep its invariants).
-   * the function returns the number of rotations, or 0 if i was not in the tree
-   * or no reotations were needed
-   */
-   public int delete(int i)
-   {
-	   return 0;	// to be replaced by student code 
-   }
-   
-   /**
-    * public int min()
-    * 
-    * Returns the smallest key in the tree, or -1 if the tree is empty
-    * is empty, returns -1;
-    * 
-    */
-   public int min()
-   {
-	   return -1; // to be replaced by student code
-   }
-   
-   /**
-    * public int max()
-    * 
-    * Returns the largest key in the tree, or -1 if the tree is empty
-    */
-   public int max()
-   {
-	   return -1; // to be replaced by student code
-   }
-   
-  /**
-   * public void arrayToTree(int[] )
-   * 
-   * inserts the array of integers to the tree.
-   * if the tree contained elements before, they should be discarded
-   * the array contains integers in ascending order.
-   * 
-   */
-  public void arrayToTree(int[] array)
-  {
-	 return; //	 to be replaced by student code
-  }
+	/**
+	 * public void insert(int i)
+	 * 
+	 * inserts the integer i into the binary tree; the tree must remain valid
+	 * (keep its invariants). the function returns the number of rotations, or 0
+	 * if i was already in the tree or no reotations were needed
+	 */
+	public int insert(int i) {
+		return 0; // to be replaced by student code
+	}
 
-   /**
-    * public int size()
-    * 
-    * Returns the number of nodes in the tree.
-    * 
-    * precondition: none
-    * postcondition: none
-    */
-   public int size()
-   {
-	   return 0; // to be replaced by student code
-   }
+	/**
+	 * public void delete(int i)
+	 * 
+	 * deletes the integer i from the binary tree, if it is there; the tree must
+	 * remain valid (keep its invariants). the function returns the number of
+	 * rotations, or 0 if i was not in the tree or no reotations were needed
+	 */
+	public int delete(int i) {
+		return 0; // to be replaced by student code
+	}
 
-  /**
-   * public class RBNode
-   * 
-   * If you wish to implement classes other than RBTree
-   * (for example RBNode), do it in this file, not in 
-   * another file 
-   *  
-   */
-  public class RBNode{
-	  /** Parent holding this node */
+	/**
+	 * public int min()
+	 * 
+	 * Returns the smallest key in the tree, or -1 if the tree is empty is
+	 * empty, returns -1;
+	 * 
+	 */
+	public int min() {
+		if (empty()) {
+			return -1;
+		}
+		RBNode current = this.root;
+		while (current.left != null) {
+			current = current.left;
+		}
+		return current.key;
+	}
+
+	/**
+	 * public int max()
+	 * 
+	 * Returns the largest key in the tree, or -1 if the tree is empty
+	 */
+	public int max() {
+		if (empty()) {
+			return -1;
+		}
+		RBNode current = this.root;
+		while (current.right != null) {
+			current = current.right;
+		}
+		return current.key;
+	}
+
+	/**
+	 * public void arrayToTree(int[] )
+	 * 
+	 * inserts the array of integers to the tree. if the tree contained elements
+	 * before, they should be discarded the array contains integers in ascending
+	 * order.
+	 * 
+	 */
+	public void arrayToTree(int[] array) {
+		return; // to be replaced by student code
+	}
+
+	/**
+	 * public int size()
+	 * 
+	 * Returns the number of nodes in the tree.
+	 * 
+	 * precondition: none postcondition: none
+	 */
+	public int size() {
+		return 0; // to be replaced by student code
+	}
+
+	/**
+	 * public class RBNode
+	 * 
+	 * If you wish to implement classes other than RBTree (for example RBNode),
+	 * do it in this file, not in another file
+	 * 
+	 */
+	public class RBNode {
+		/** Parent holding this node */
 		private RBNode parent;
-		
+
 		/** Left son */
 		private RBNode left;
-		
+
 		/** Right son */
 		private RBNode right;
-		
+
 		/** Ordered key */
 		private int key;
-		
+
 		/** Whether this node is black or red otherwise */
 		private boolean isBlack;
 
-		public RBNode(RBNode parent, RBNode left, RBNode right, int key, boolean isBlack) {
+		public RBNode(RBNode parent, RBNode left, RBNode right, int key,
+				boolean isBlack) {
 			this.parent = parent;
 			this.left = left;
 			this.right = right;
 			this.key = key;
 			this.isBlack = isBlack;
 		}
-		
+
 		/**
 		 * @return the parent
 		 */
@@ -143,7 +148,8 @@ public class RBTree {
 		}
 
 		/**
-		 * @param parent the parent to set
+		 * @param parent
+		 *            the parent to set
 		 */
 		public void setParent(RBNode parent) {
 			this.parent = parent;
@@ -157,7 +163,8 @@ public class RBTree {
 		}
 
 		/**
-		 * @param left the left to set
+		 * @param left
+		 *            the left to set
 		 */
 		public void setLeft(RBNode left) {
 			this.left = left;
@@ -172,7 +179,8 @@ public class RBTree {
 		}
 
 		/**
-		 * @param right the right to set
+		 * @param right
+		 *            the right to set
 		 */
 		public void setRight(RBNode right) {
 			this.right = right;
@@ -187,7 +195,8 @@ public class RBTree {
 		}
 
 		/**
-		 * @param key the key to set
+		 * @param key
+		 *            the key to set
 		 */
 		public void setKey(int key) {
 			this.key = key;
@@ -201,35 +210,35 @@ public class RBTree {
 		}
 
 		/**
-		 * @param isBlack the isBlack to set
+		 * @param isBlack
+		 *            the isBlack to set
 		 */
 		public void setBlack(boolean isBlack) {
 			this.isBlack = isBlack;
 		}
-		
-		
+
 		/**
-		 * replaces the current node with its father
-		 * if this.parent.parent is Null - returns false
+		 * replaces the current node with its father if this.parent.parent is
+		 * Null - returns false
 		 * 
-		 * @throws InvalidOperationException If no parent to replace
+		 * @throws InvalidOperationException
+		 *             If no parent to replace
 		 */
 		private void moveUp() {
 			if ((this.parent == null) || (this.parent.getParent() == null)) {
 				return;
 			}
-			
+
 			if (this.parent == this.parent.getParent().getLeft()) {
 				this.parent.getParent().setLeft(this);
 			} else {
 				this.parent.getParent().setRight(this);
 			}
 		}
-		
+
 		/**
-		 * Takes current Node's left son, moves it up,
-		 * it's right son to be it's parent's left son
-		 * and the parent to be it's right son
+		 * Takes current Node's left son, moves it up, it's right son to be it's
+		 * parent's left son and the parent to be it's right son
 		 */
 		public void rotateRight() {
 			RBNode y = this.left;
@@ -240,14 +249,13 @@ public class RBTree {
 			this.setLeft(y.getRight());
 			y.setRight(this);
 		}
-		
+
 		/**
-		 * Takes current Node's right son, moves it up,
-		 * it's left son to be it's parent's right son
-		 * and the parent to be it's left son
+		 * Takes current Node's right son, moves it up, it's left son to be it's
+		 * parent's right son and the parent to be it's left son
 		 */
 		public void rotateLeft() {
-			
+
 			RBNode right = this.right;
 			if (right == null) {
 				return;
@@ -257,13 +265,10 @@ public class RBTree {
 			right.setLeft(this);
 		}
 
-  }
-  
-  /**
- * @original author Shai Vardi
- * Modified for semester 2013 b
- */
+	}
+
+	/**
+	 * @original author Shai Vardi Modified for semester 2013 b
+	 */
 
 }
-  
-
