568dcdf47434972b135196ea1cac2aa7fa9925c9
shiragold <shira14@gmail.com>
2013-05-24 23:59:02 +0300

    Skeleton

---
+++ BinomialHeap/src/system/BinomialHeap.java
@@ -0,0 +1,118 @@
+package system;
+/**
+ * BinomialHeap
+ *
+ * An implementation of lazy binomial heap over non-negative integers.
+ */
+public class BinomialHeap
+{
+
+   /**
+    * public boolean empty()
+    *
+    * precondition: none
+    *
+    * The method returns true if and only if the heap
+    * is empty.
+    *
+    */
+    public boolean empty()
+    {
+    	return false; // should be replaced by student code
+    }
+
+   /**
+    * public void insert(int value)
+    *
+    * Insert value into the heap
+    *
+    */
+    public void insert(int value)
+    {
+    	return; // should be replaced by student code
+    }
+
+   /**
+    * public void deleteMin()
+    *
+    * Delete the minimum value.
+    * Return the number of linking actions that occured in the process.
+    *
+    */
+    public int deleteMin()
+    {
+     	return 42; // should be replaced by student code
+    }
+
+   /**
+    * public int findMin()
+    *
+    * Return the minimum value
+    *
+    */
+    public int findMin()
+    {
+    	return 42;// should be replaced by student code
+    }
+
+   /**
+    * public void meld (BinomialHeap heap2)
+    *
+    * Meld the heap with heap2
+    *
+    */
+    public void meld (BinomialHeap heap2)
+    {
+    	  return; // should be replaced by student code
+    }
+
+   /**
+    * public int size()
+    *
+    * Return the number of elements in the heap
+    *
+    */
+    public int size()
+    {
+    	return 42; // should be replaced by student code
+    }
+
+
+   /**
+    * public static int sortArray(int[] array)
+    *
+    * Sort an array by using insert and deleteMin actions on a new heap.
+    * Return the number of linking actions that occured in the process.
+    *
+    */
+    public static int sortArray(int[] array)
+    {
+        return 42; //	 to be replaced by student code
+    }
+
+   /**
+    * public int[] treesRanks()
+    *
+    * Return an array containing the ranks of the trees that represent the heap
+    * in ascending order.
+    *
+    */
+    public int[] treesRanks()
+    {
+        int[] arr = new int[42]; //
+        return arr; //	 to be replaced by student code
+    }
+
+   /**
+    * public class BinomialHeapTree
+    *
+    * If you wish to implement classes other than BinomialHeap
+    * (for example BinomialHeapTree), do it in this file, not in
+    * another file
+    *
+    */
+    public class BinomialHeapTree{
+
+    }
+
+}
