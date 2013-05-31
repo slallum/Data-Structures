a2321979a2d9190d1957153492207819fdad2d85
slallum <horowitz.ofer@gmail.com>
2013-05-31 16:56:03 +0300

    changed test

--- BinomialHeap/src/test/TestBinomialTree.java
+++ BinomialHeap/src/test/TestBinomialTree.java
@@ -1,7 +1,14 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import system.BinomialHeap;
 
@@ -19,7 +26,7 @@ public class TestBinomialTree {
 		heap.insert(4);
 		assertEquals(4, heap.size());
 	}
-	
+
 	@Test
 	public void testFindMinSimple() {
 		BinomialHeap heap = new BinomialHeap();
@@ -29,7 +36,7 @@ public class TestBinomialTree {
 		heap.insert(4);
 		assertEquals(4, heap.findMin());
 	}
-	
+
 	@Test
 	public void testDelMin() {
 		BinomialHeap heap = new BinomialHeap();
@@ -42,10 +49,44 @@ public class TestBinomialTree {
 		heap.deleteMin();
 		assertEquals(10, heap.findMin());
 	}
-	
+
 	@Test
 	public void testUltimate() {
+		List<Integer> randomNumbers = getRandomNumbersList(4);
+
+		BinomialHeap heap = new BinomialHeap();
+
+		int myMin = Integer.MAX_VALUE;
+		for (int n : randomNumbers) {
+			heap.insert(n);
+			myMin = Math.min(myMin, n);
+			assertEquals(myMin, heap.findMin());
+		}
+
+		List<Integer> newSortedList = new ArrayList<Integer>();
+
+		while (heap.size() > 0) {
+			newSortedList.add(heap.findMin());
+			heap.deleteMin();
+		}
 		
+		assertTrue(isSorted(newSortedList));
+	}
+
+	private boolean isSorted(List<Integer> newSortedList) {
+		Integer[] beforeArray = newSortedList.toArray(new Integer[newSortedList.size()]);
+		Collections.sort(newSortedList);
+		Integer[] afterArray = newSortedList.toArray(new Integer[newSortedList.size()]);
+		return Arrays.equals(beforeArray, afterArray);
+	}
+
+	private List<Integer> getRandomNumbersList(int numbersCount) {
+		List<Integer> numbers = new ArrayList<Integer>();
+		Random rand = new Random();
+		for (int i = 0; i < numbersCount; i++) {
+			numbers.add(rand.nextInt(100)); // 2**30
+		}
+		return numbers;
 	}
 
 }
