0601e67e2f2fd86495149c2950975a0404f0a0f1
shiragold <shiragold@mail.tau.ac.il>
2013-12-22 17:39:47 +0200

    Also print works

--- c/Ex3/src/scoring.test.c
+++ c/Ex3/src/scoring.test.c
@@ -6,33 +6,36 @@
  */
 
 #include "scoring.h"
+#include "board.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void testConnect4Scoring() {
 
+	board_t board;
 	int i;
-	int **board = calloc(6, sizeof(int*));
+	int **cells = calloc(6, sizeof(int*));
 	for (i = 0; i < 6; i++) {
-		board[i] = calloc(7, sizeof(int));
+		cells[i] = calloc(7, sizeof(int));
 	}
-	board[1][1] = 1;
-	board[2][1] = 1;
-	board[3][1] = -1;
-	board[3][2] = -1;
-	board[3][3] = 1;
-	board[4][1] = 1;
-	board[4][2] = -1;
-	board[4][3] = -1;
-	board[5][1] = 1;
-	board[5][2] = 1;
-	board[5][3] = -1;
-	board[5][4] = 1;
-	board[5][5] = -1;
-	board[5][6] = -1;
-
-	printf("%d", connect4_scoring(board, 6, 7));
+	cells[1][1] = 1;
+	cells[2][1] = 1;
+	cells[3][1] = -1;
+	cells[3][2] = -1;
+	cells[3][3] = 1;
+	cells[4][1] = 1;
+	cells[4][2] = -1;
+	cells[4][3] = -1;
+	cells[5][1] = 1;
+	cells[5][2] = 1;
+	cells[5][3] = -1;
+	cells[5][4] = 1;
+	cells[5][5] = -1;
+	cells[5][6] = -1;
+	board = (board_t){.cells = cells,.n = 6, .m = 7, .scoring_func=*connect4_scoring, .make_move=NULL};
+	print_board(&board);
+	printf("%d", get_score(&board));
 }
 
 int main() {
